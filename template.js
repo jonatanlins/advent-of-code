76691fd154a41c1de1418e67c27bbe9e5006c14f
Jonatan Lins <jonatanpinheiro99@gmail.com>
2024-12-01 04:47:29 -0300

    add template

---
+++ template.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+const result = input;
+
+console.log(`The result is ${result}`);
